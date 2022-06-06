b881983dd1947f1feef95816e6f196388fa00754
Ben Kiboma <omayioben@gmail.com>
2022-06-06 21:24:31 +0300

    copied code for PR assignment

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Kibs-To-Do-List-PR
+# Kibs-To-Do-List
+
+> This is a to do list app using HTML, CSS, JavaScript and webpack
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- Webpack
+
+## Live Demo
+
+[Live Demo Link](https://kiboma2021.github.io/Kibs-To-Do-List/)
+
+## Getting Started
+
+- Clone the repository
+- Change directory into the project folder
+- Open project with text editor to view changes
+
+### Prerequisites
+- Code editor
+- Knowledge in HTML, CSS and JavaScript
+- Git
+- Node
+
+### Install
+- Install dependencies npm install
+
+- The project is build with webpack so you need to start the dev server with the following command npm run start
+
+### Usage
+- Make changes to the index.html and submit pull requests
+
+## Authors
+
+👤 **Ben Omayio**
+
+- GitHub: [@kiboma2021](https://github.com/kiboma2021)
+- Twitter: [@BenOmayio](https://twitter.com/omayiobenj)
+- LinkedIn: [@BenOmayio](https://www.linkedin.com/in/ben-omayio-74622469/)
+
+
+## 🤝 Contributing
+
+- Contributions, issues, and feature requests are welcome!
+
+- Feel free to check the issues page.
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Project Design and inspiration from [Microverse Bootcamp](https://www.microverse.org)
---
+++ src/index.js
@@ -0,0 +1,179 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import './style.css';
+import updateLocal from './status_update.js'
+
+//Reference HTML
+const myInput = document.querySelector('input');
+const clearContent = document.querySelector('#clear-content');
+const todoList = document.querySelector('#display-list');
+
+//Template class object
+class myObject {
+  constructor (description, complited, index) {
+    this.description = description;
+    this.complited = complited;
+    this.index = index;
+  }
+}
+
+const myTodoList = [];
+
+const addTodo = toDovalue => {
+  const toDocontainer = document.createElement('div');
+  toDocontainer.className = 'toDocontainer';
+  toDocontainer.innerHTML += `
+  <input type="checkbox" class="checkbox">
+  <span>${toDovalue}</span>
+  <span class="edit-to-do"> <i class="fa fa-ellipsis-v"></i></span>
+  <span class="remove-icon"><i class="fa fa-trash-alt" ></i></span>
+  `
+  todoList.appendChild(toDocontainer);
+
+
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach(i => {
+    i.addEventListener('change', () => {
+      i.parentElement.classList.toggle('checkedContainer');
+      i.nextElementSibling.classList.toggle('check-to-do');
+      i.parentElement.lastElementChild.classList.toggle('trash-active');
+      i.parentElement.lastElementChild.previousElementSibling.classList.toggle('edit-disabled');
+      updateLocal();
+    })
+  }) 
+  
+  //Add items to Local Storage
+  const object = new myObject (toDovalue, false, checkbox.length )
+  myTodoList.push(object);
+  localStorage.setItem ('List', JSON.stringify(myTodoList));
+
+  //Edit todo list
+  const editIcons = document.querySelectorAll('.edit-to-do');
+  editIcons.forEach (i => {
+    i.addEventListener('click', () => {
+      i.parentElement.classList.add('checkedContainer');
+      editTodo(toDocontainer, i.previousElementSibling);
+    })
+  })
+
+  //Remove from the list
+  const removeList = document.querySelectorAll('.remove-icon');
+  removeList.forEach (i => {
+    i.addEventListener('click', () => {
+      removeTodo(i.parentElement);
+    })
+  })
+};
+
+//Add editTodo function
+const editTodo = (toDocontainer, todo) => {
+  const editInput = document.createElement('input');
+  editInput.type = "text";
+  editInput.className = "editInput";
+  editInput.value = todo.textContent;
+  toDocontainer.replaceChild(editInput, todo);
+
+  //add event listerner to edit input
+  editInput.addEventListener ('keypress', e => {
+    if (e.key === 'Enter') {
+      const editContainers = document.querySelectorAll('.toDocontainer');
+      const localData = JSON.parse (localStorage.getItem('List'));
+      for (let i=0; i<editContainers.length; i++) {
+        if (editContainers[i].classList.contains('checkedContainer')) {
+          localData[i].description = editInput.value;
+          localStorage.setItem ('List', JSON.stringify(localData));
+        }
+      }
+      editInput.parentElement.classList.remove('checkedContainer');
+      toDocontainer.replaceChild(todo, editInput);
+      todo.textContent = editInput.value;
+    }
+  })
+}
+
+//Remove Items from to do list
+const removeTodo = (todo) => {
+  todoList.removeChild(todo);
+  let count = 1;
+  const localData = JSON.parse(localStorage.getItem('List'));
+  const data = Array.from(localData).filter (i => i.complited === false);
+  data.map (i => i.index = count++);
+  localStorage.setItem ('List', JSON.stringify(data));
+  window.location.reload();
+}
+
+//Add event lister when enter is clicked while in input field
+myInput.addEventListener ('keypress', e => {
+  if (e.key === 'Enter' && myInput.value ) {
+    addTodo(myInput.value);
+    myInput.value = null;
+  }
+})
+
+//Get data from local storage
+const getFromLocal = () => {
+  const data = JSON.parse(localStorage.getItem('List'));
+  data.map (i => {
+    myTodoList.push(i);
+    const toDocontainer = document.createElement('div');
+    toDocontainer.className = 'toDocontainer';
+    toDocontainer.innerHTML += `
+    <input type="checkbox" class="checkbox">
+    <span class="description">${i.description}</span>
+    <span class="edit-to-do"> <i class="fa fa-ellipsis-v"></i></span>
+    <span class="remove-icon"><i class="fa fa-trash-alt" ></i></span>
+    `
+    todoList.appendChild(toDocontainer);
+
+    //Edit todo list
+    const editIcons = document.querySelectorAll('.edit-to-do');
+    editIcons.forEach (i => {
+      i.addEventListener('click', () => {
+        i.parentElement.classList.add('checkedContainer');
+        editTodo(toDocontainer, i.previousElementSibling);
+      })
+    })
+  })
+  //Get the checkbox
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach(i => {
+    i.addEventListener('change', () => {
+      i.parentElement.classList.toggle('checkedContainer');
+      i.nextElementSibling.classList.toggle('check-to-do');
+      i.parentElement.lastElementChild.classList.toggle('trash-active');
+      i.parentElement.lastElementChild.previousElementSibling.classList.toggle('edit-disabled');
+      updateLocal();
+    })
+  })
+
+  //Remove from the list
+  const removeList = document.querySelectorAll('.remove-icon');
+  removeList.forEach (i => {
+    i.addEventListener('click', () => {
+      removeTodo(i.parentElement);
+    })
+  })
+
+  //Now send data to local storage
+  localStorage.setItem('List',JSON.stringify(myTodoList));
+}
+
+window.addEventListener('load',getFromLocal);
+
+//Clear All
+const clearAll = () => {
+  const localData = JSON.parse(localStorage.getItem('List'));
+  const todo_container = document.querySelectorAll('.toDocontainer');
+  todo_container.forEach (i => {
+    if (i.classList.contains('checkedContainer')) {
+      removeTodo(i);
+    }
+  })
+  let count = 1;
+  const data = Array.from(localData).filter(i => i.complited == false);
+  data.map (i => i.index = count++);
+  localStorage.setItem ('List', JSON.stringify(data));
+  window.location.reload();
+}
+
+clearContent.addEventListener ('click', clearAll);
